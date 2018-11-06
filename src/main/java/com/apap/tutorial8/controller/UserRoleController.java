a69916ff6dbd4fca826e16fe8d09b94aeabb4d84
Nikmah Salsabila <nikmahsalsabila28@gmail.com>
2018-11-06 21:35:06 +0700

    nmr 3

--- src/main/java/com/apap/tutorial8/controller/UserRoleController.java
+++ src/main/java/com/apap/tutorial8/controller/UserRoleController.java
@@ -1,5 +1,7 @@
 package com.apap.tutorial8.controller;
 
+import java.util.regex.Pattern;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,10 +23,27 @@ public class UserRoleController {
 	@Autowired
 	private UserRoleService userService;
 
+	public boolean validatePassword (String pass) {
+		if(pass.length() >= 8 && Pattern.compile("[a-zA-Z]").matcher(pass).find() && Pattern.compile("[0-9]").matcher(pass).find()) {
+			return true;
+		}
+		return false;
+	}
+	
 	@RequestMapping( value = "/addUser", method = RequestMethod.POST)
-	private String addUserSubmit(@ModelAttribute UserRoleModel user) {
-		userService.addUser(user);
-		return "home";
+	private ModelAndView addUserSubmit(@ModelAttribute UserRoleModel user, RedirectAttributes redirect) {
+		String message="";
+		
+		if(this.validatePassword(user.getPassword())) {
+			userService.addUser(user);
+			message= null;
+		}
+		else {
+			message="password minimal terdiri dari 8 kata dengan minimal 1 huruf dan angka";
+		}
+		ModelAndView redirects = new ModelAndView("redirect:/");
+		redirect.addFlashAttribute("msg", message);
+		return redirects;
 	}
 	
 	@RequestMapping( value = "/updatePass")
@@ -36,26 +55,31 @@ public class UserRoleController {
 	private ModelAndView updatePasswordSubmit(@ModelAttribute PasswordModel pass, Model model, RedirectAttributes redir) {
 		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		UserRoleModel user = userService.findUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+		
 		String message = "";
 		if (pass.getConfirmPassword().equals(pass.getNewPassword())) {
 			if (passwordEncoder.matches(pass.getOldPassword(), user.getPassword())) {
-				userService.changePassword(user, pass.getNewPassword());
-				message = "password berhasil diubah";
-
-				System.out.println("PESAN  cocok ama DB: " + message);
+				if(this.validatePassword(pass.getNewPassword())) {
+					String passBaru = pass.getNewPassword();
+					userService.changePassword(user, passBaru);
+					message = "password berhasil diubah";
+				}
+				else {
+					message="password harus terdiri dari 8 karakter dengan minimal 1 angka dan huruf";
+				}
+				//System.out.println("PESAN  cocok ama DB: " + message);
 			}
 			else {
 				message = "password lama anda salah";
-
-				System.out.println("PESAN Gak COcok AMa DB : " + message);
+				//System.out.println("PESAN Gak COcok AMa DB : " + message);
 			}	
 		}
 		else {
 			message = "password baru tidak sesuai";
-
-			System.out.println("PESAN beda konfirmasi : " + message);
+			//System.out.println("PESAN beda konfirmasi : " + message);
 		}
-		System.out.println("PESAN  terakhir :\"\"( : " + message);
+		
+		//System.out.println("PESAN  terakhir :\"\"( : " + message);
 		ModelAndView modelAndView = new ModelAndView("redirect:/user/updatePass");
 		redir.addFlashAttribute("msg",message);
 		return modelAndView;
